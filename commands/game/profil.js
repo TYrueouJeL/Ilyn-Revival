749c7eccda04bdf0fba0f9c24c833de60db79123
TYrueouJeL <guerinremi.pro@gmail.com>
2025-03-31 16:14:35 +0200

    delete profil command and profilSequelize -> profil

--- commands/game/profil.js
+++ commands/game/profil.js
@@ -1,5 +1,9 @@
-const connection = require('../../events/database/connection');
 const { EmbedBuilder } = require('discord.js');
+const sequelize = require('../../events/database/sequelizeConnection');
+const Adventurer = require('../../models/Adventurer');
+const Personage = require('../../models/Personage');
+const Class = require('../../models/Class');
+const Level = require('../../models/Level');
 
 module.exports = {
     name: 'profil',
@@ -8,69 +12,56 @@ module.exports = {
     async run(client, message) {
         const discordId = message.author.id;
 
-        const verificationQuery = `
-            SELECT
-                Adventurer.IdDiscord
-            FROM Adventurer
-            WHERE Adventurer.IdDiscord = ?
-        `
+        try {
+            // Vérifier si l'aventurier existe
+            const adventurer = await Adventurer.findOne({
+                where: { IdDiscord: discordId },
+                include: [
+                    {
+                        model: Personage,
+                        include: [
+                            { model: Class, attributes: ['Name'] },
+                            { model: Level, attributes: ['Level', 'ExperienceRequired'] }
+                        ],
+                        attributes: ['Name', 'HealthPoints', 'MaxHealthPoints', 'Attack', 'Defense']
+                    }
+                ],
+                attributes: ['Experience', 'Gold']
+            });
 
-        connection.query(verificationQuery, [discordId], (error, result) => {
-            if (error) {
-                console.error('Erreur lors de la vérification de l\'aventurier:', error);
-                return message.channel.send('Une erreur est survenue lors de la vérification de l\'aventurier.');
-            }
-
-            if (result.length === 0) {
+            if (!adventurer) {
                 return message.channel.send('Vous n\'avez pas d\'aventurier. Veuillez en créer un avec la commande `!startAdventure`.');
             }
-            
-            const query = `
-SELECT
-    personage.Name AS Name,
-    class.Name AS ClassName,
-    level.Level AS AdventurerLevel,
-    adventurer.Experience AS Experience,
-    level.ExperienceRequired AS ExperienceRequired,
-    personage.HealthPoints AS HealthPoints,
-    personage.MaxHealthPoints AS MaxHealthPoints,
-    personage.Attack AS Attack,
-    personage.Defense AS Defense,
-    adventurer.Gold AS Gold
-FROM adventurer
-JOIN personage ON adventurer.IdPersonage = personage.IdPersonage
-JOIN class ON personage.IdClass = class.IdClass
-JOIN level ON personage.IdLevel = level.IdLevel
-WHERE adventurer.IdDiscord = ?
-            `;
 
-            connection.query(query, [discordId], (error, result) => {
-                if (error) {
-                    console.error('Erreur lors de la récupération des informations de l\'aventurier:', error);
-                    return message.channel.send('Une erreur est survenue lors de la récupération des informations de l\'aventurier.');
-                }
+            // Extraire les données nécessaires
+            const {
+                Personage: { Name, Class: { Name: ClassName }, Level: { Level: AdventurerLevel, ExperienceRequired }, HealthPoints, MaxHealthPoints, Attack, Defense },
+                Experience,
+                Gold
+            } = adventurer;
 
-                const adventurerInfo = result[0];
+            // Créer un embed avec les informations
+            const adventurerEmbed = new EmbedBuilder()
+                .setColor('Blue')
+                .setTitle(`${Name}`)
+                .setAuthor({ name: message.author.username, iconURL: message.author.displayAvatarURL() })
+                .setDescription(`Voici vos informations d'aventurier :`)
+                .setThumbnail(message.author.displayAvatarURL())
+                .addFields(
+                    { name: 'Classe', value: ClassName, inline: true },
+                    { name: 'Points de vie', value: `${HealthPoints} / ${MaxHealthPoints} ❤️`, inline: true },
+                    { name: 'Dégâts', value: `${Attack} ⚔️`, inline: true },
+                    { name: 'Défense', value: `${Defense} 🛡️`, inline: true },
+                    { name: 'Gold', value: `${Gold} 💰`, inline: true },
+                    { name: 'Niveau', value: `${AdventurerLevel} ⬆️`, inline: true },
+                    { name: 'Points d\'expérience', value: `${Experience} / ${ExperienceRequired} ⭐`, inline: true },
+                    { name: 'Prochain niveau', value: `${ExperienceRequired - Experience} points d\'expérience restants ⭐`, inline: true },
+                );
 
-                const adventurerEmbed = new EmbedBuilder()
-                    .setColor('Blue')
-                    .setTitle(`${adventurerInfo.Name}`)
-                    .setAuthor({ name: message.author.username, iconURL: message.author.displayAvatarURL() })
-                    .setDescription(`Voici vos informations d'aventurier :`)
-                    .setThumbnail(message.author.displayAvatarURL())
-                    .addFields(
-                        { name: 'Classe', value: adventurerInfo.ClassName, inline: true },
-                        { name: 'Points de vie', value: `${adventurerInfo.HealthPoints} / ${adventurerInfo.MaxHealthPoints} ❤️`, inline: true },
-                        { name: 'Dégâts', value: `${adventurerInfo.Attack} ⚔️`, inline: true },
-                        { name: 'Défense', value: `${adventurerInfo.Defense} 🛡️`, inline: true },
-                        { name: 'Gold', value: `${adventurerInfo.Gold} 💰`, inline: true },
-                        { name: 'Niveau', value: `${adventurerInfo.AdventurerLevel} ⬆️`, inline: true },
-                        { name: 'Points d\'expérience', value: `${adventurerInfo.Experience} / ${adventurerInfo.ExperienceRequired} ⭐`, inline: true },
-                        { name: 'Prochain niveau', value: `${adventurerInfo.ExperienceRequired - adventurerInfo.Experience} points d\'expérience restants ⭐`, inline: true },
-                    )
-                
-                message.channel.send({ embeds: [adventurerEmbed] });
-            });
-        })
+            message.channel.send({ embeds: [adventurerEmbed] });
+        } catch (error) {
+            console.error('Erreur lors de la récupération des informations de l\'aventurier:', error);
+            message.channel.send('Une erreur est survenue lors de la récupération des informations de l\'aventurier.');
+        }
     }
 };
