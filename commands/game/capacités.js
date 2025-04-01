40a442cb48490a3d3ecef43c57d3baaa0ac81e14
TYrueouJeL <guerinremi.pro@gmail.com>
2025-04-01 22:38:28 +0200

    update capacités command orm

--- commands/game/capacités.js
+++ commands/game/capacités.js
@@ -1,5 +1,10 @@
-const connection = require('../../events/database/connection');
 const { EmbedBuilder } = require('discord.js');
+const Adventurer = require('../../models/Adventurer');
+const Personage = require('../../models/Personage');
+const Class = require('../../models/Class');
+const Ability = require('../../models/Ability');
+const Level = require('../../models/Level');
+const { Sequelize } = require('../../events/database/sequelizeConnection');
 
 module.exports = {
     name: 'capacités',
@@ -7,71 +12,68 @@ module.exports = {
 
     async run(client, message) {
         const discordId = message.author.id;
-        
-        const verificationQuery = `
-        SELECT
-        Adventurer.IdDiscord
-        FROM Adventurer
-        WHERE Adventurer.IdDiscord = ?
-        `
-        
-        connection.query(verificationQuery, [discordId], (error, result) => {
-            if (error) {
-                console.error('Erreur lors de la vérification de l\'aventurier:', error);
-                return message.channel.send('Une erreur est survenue lors de la vérification de l\'aventurier.');
-            }
-            
-            if (result.length === 0) {
-                return message.channel.send('Vous n\'avez pas d\'aventurier. Veuillez en créer un avec la commande `!startAdventure`.');
-            }
-            
-            const query = `
-SELECT Ability.Name AS AbilityName, ability.Description AS AbilityDescription, level.Level AS AbilityLevel, ability.Damage AS AbilityDamage, ability.Regen AS AbilityRegen, ability.Shield AS AbilityShield, Ability.Experience AS AbilityExperience, Level.ExperienceRequired AS AbilityExperienceRequired
-FROM Adventurer
-JOIN Personage ON Adventurer.IdPersonage = Personage.IdPersonage
-JOIN Level AS PersonageLevel ON Personage.IdLevel = PersonageLevel.IdLevel
-JOIN Class ON Personage.IdClass = Class.IdClass
-JOIN Ability ON Class.IdClass = Ability.IdClass
-JOIN Level ON Ability.IdLevel = Level.IdLevel
-WHERE Adventurer.IdDiscord = ?
-  AND PersonageLevel.Level >= Ability.LevelRequired;
-            `;
-            
-            connection.query(query, [discordId], (error, result) => {
-                if (error) {
-                    console.error('Erreur lors de la récupération des informations des capacités de l\'aventurier:', error);
-                    return message.channel.send('Une erreur est survenue lors de la récupération des informations des capacités de l\'aventurier.');
-                }
 
-                if (result.length === 0) {
-                    return message.channel.send('Vous n\'avez pas encore de capacités.');
-                }
-
-                const adventurerEmbed = new EmbedBuilder()
-                    .setColor('Blue')
-                    .setTitle('Vos capacités')
-                    .setAuthor({ name: message.author.username, iconURL: message.author.displayAvatarURL() })
-                    .setDescription(`Voici vos capacités :`)
-                    .setThumbnail(message.author.displayAvatarURL())
-                    .setTimestamp();
-
-                result.forEach(ability => {
-                    adventurerEmbed.addFields({
-                        name: ability.AbilityName,
-                        value: `
-Description : ${ability.AbilityDescription}
-Niveau : ${ability.AbilityLevel} ⬆️
-Expérience : ${ability.AbilityExperience} / ${ability.AbilityExperienceRequired} ⭐
-Dégâts : ${ability.AbilityDamage} ⚔️
-Régénération : ${ability.AbilityRegen} 💚
-Bouclier : ${ability.AbilityShield} 🛡️
-`,
-                        inline: false
-                    });
-                })
-
-                message.channel.send({ embeds: [adventurerEmbed] });
+        try {
+            const adventurer = await Adventurer.findOne({
+                where: { IdDiscord: discordId },
+                include: [
+                    {
+                        model: Personage,
+                        include: [
+                            {
+                                model: Class,
+                                include: [
+                                    {
+                                        model: Ability,
+                                        include: [
+                                            {
+                                                model: Level,
+                                                attributes: ['Level', 'ExperienceRequired']
+                                            }
+                                        ],
+                                        attributes: ['Name', 'Description', 'Damage', 'Regen', 'Shield', 'Experience', 'LevelRequired']
+                                    }
+                                ]
+                            }
+                        ],
+                        attributes: ['Name', 'IdLevel'],
+                    }
+                ]
             });
-        })
+
+            if (adventurer && adventurer.Personage && adventurer.Personage.Class) {
+                const personageLevel = adventurer.Personage.IdLevel;
+                const abilities = adventurer.Personage.Class.Abilities || [];
+
+                const filteredAbilities = abilities.filter(ability => ability.Level.Level <= personageLevel);
+
+                const abilitiesEmbed = new EmbedBuilder()
+                    .setColor('Gold')
+                    .setTitle(`${adventurer.Personage.Name}`)
+                    .setAuthor({ name: message.author.username, iconURL: message.author.displayAvatarURL() })
+                    .setDescription('Voici vos capacités :')
+                    .setThumbnail(message.author.displayAvatarURL())
+                    .addFields(
+                        filteredAbilities.map(ability => ({
+                            name: ability.Name,
+                            value: `
+Description: ${ability.Description}
+Expérience: ${ability.Experience} / ${ability.Level.ExperienceRequired} ⭐
+Dégâts: ${ability.Damage} ⚔️
+Régénération: ${ability.Regen} 💚
+Bouclier: ${ability.Shield} 🛡️
+`,
+                            inline: false
+                        }))
+                    );
+
+                message.channel.send({ embeds: [abilitiesEmbed] });
+            } else {
+                message.channel.send('Vous n\'avez pas encore de capacités.');
+            }
+        } catch (error) {
+            console.error('Erreur lors de la récupération des capacités de l\'aventurier:', error);
+            message.channel.send('Une erreur est survenue lors de la récupération des capacités de l\'aventurier.');
+        }
     }
 }
